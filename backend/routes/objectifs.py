399d89f22620986dc705cdeed28bd8dca8da28a3
AymanMidan <aymanmidan2005@gmail.com>
2025-01-14 23:03:39 +0100

    Update objectifs.py

--- backend/routes/objectifs.py
+++ backend/routes/objectifs.py
@@ -4,16 +4,16 @@ from config import db, cursor
 objectifs_bp = Blueprint('objectifs', __name__)
 
 @objectifs_bp.route('/objectifs', methods=['POST'])
+@objectifs_bp.route('/objectifs', methods=['POST'])
 def add_objectif():
     data = request.json
     utilisateur_id = data['utilisateur_id']
-    nombre_seances = data['nombre_seances']  # Récupérer le nombre de séances
-    calories_a_bruler = data['calories_a_bruler']  # Récupérer les calories à brûler
-    objectif_sante = data['objectif_sante']
-    notes = data['notes']
+    nombre_seances = data.get('nombre_seances')  # Utilisez .get() pour éviter les erreurs si le champ est manquant
+    calories_a_bruler = data.get('calories_a_bruler')
+    objectif_sante = data.get('objectif_sante')
+    notes = data.get('notes')
 
     try:
-        # Insérer tous les champs dans la base de données
         cursor.execute(
             "INSERT INTO Objectifs (utilisateur_id, nombre_seances, calories_a_bruler, objectif_sante, notes) VALUES (%s, %s, %s, %s, %s)",
             (utilisateur_id, nombre_seances, calories_a_bruler, objectif_sante, notes)
